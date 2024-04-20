8b363ff932dcaa0a204e2196f932f3dcb883efef
Marcelo <marceloboaria@gmail.com>
2024-04-19 21:19:00 -0300

    Adicionei as últimas atualizações feitas em aula, mas ainda preciso entender e corrigir um erro específico que apareceu

--- src/main/java/com/clickcar/clickcarback/controllers/UserController.java
+++ src/main/java/com/clickcar/clickcarback/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.clickcar.clickcarback.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.clickcar.clickcarback.dtos.users.UserInput;
+import com.clickcar.clickcarback.dtos.users.UserOutput;
 import com.clickcar.clickcarback.entities.User;
 import com.clickcar.clickcarback.repositories.UserRepository;
 
@@ -23,28 +27,59 @@ public class UserController {
     private UserRepository repository;
 
     @GetMapping
-    public List<User> list() {
-        return repository.findAll();
+    public ResponseEntity<List<User>> list() {
+        List<User> list = repository.findAll();
+        // return new ResponseEntity<>(list, HttpStatus.OK);
+        return ResponseEntity.ok(list);
     }
 
     @PostMapping
-    public User create(@RequestBody User user) {
-        return repository.save(user);
+    public UserOutput create(@RequestBody UserInput user) {
+
+        User userToSave = new User();
+
+        userToSave.setName(user.getName());
+        userToSave.setEmail(user.getEmail());
+        userToSave.setCpf(user.getCpf());
+        userToSave.setPhone(user.getPhone());
+        userToSave.setPassword(user.getPassword());
+        userToSave.setAddress(user.getAddress());
+
+        User userSaved = repository.save(userToSave);
+
+        UserOutput output = new UserOutput(
+
+            userSaved.getId(),
+            userSaved.getName(),
+            userSaved.getEmail(),
+            userSaved.getCpf(),
+            userSaved.getPhone(),
+            userSaved.getAddress());
+
+            return new ResponseEntity<>(output, HttpStatus.CREATED);
+
     }
 
     @GetMapping("/{id}") // Entre chaves para indicar que é um Parâmetro Variável!
-    public User read(@PathVariable Long id) {
-        return repository.findById(id).get();
+    public ResponseEntity<User> read(@PathVariable Long id) {
+        User user = repository.findById(id).get();
+        // return new ResponseEntity<>(user, HttpStatus.OK);
+        return ResponseEntity.ok(user);
     }
 
     @PutMapping
-    public User update(@RequestBody User user) {
-        return repository.save(user);
+    public ResponseEntity<User> update(@RequestBody User user) {
+        User userUpdated = repository.save(user);
+        // return new ResponseEntity<>(userUpdated, HttpStatus.OK);
+        return ResponseEntity.ok(userUpdated);
     }
 
+    @SuppressWarnings("rawtypes") // Para fazer o compilador não emitir warnings.
     @DeleteMapping("/{id}") // Entre chaves para indicar que é um Parâmetro Variável!
-    public void delete(@PathVariable Long id) {
+    public ResponseEntity delete(@PathVariable Long id) {
         repository.deleteById(id);
+        // return new ResponseEntity(HttpStatus.NO_CONTENT);
+        return ResponseEntity.noContent().build();
     }
 
     /*
