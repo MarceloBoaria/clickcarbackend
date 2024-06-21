a34a84983b037e7d1b0cc980d4dd7e0687f9c933
Marcelo <marceloboaria@gmail.com>
2024-06-20 22:09:48 -0300

    Avisos retirados

--- src/main/java/com/clickcar/clickcarback/controllers/UserController.java
+++ src/main/java/com/clickcar/clickcarback/controllers/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
