cf31b2b1f702c87335ebd24774c52f747a301ff5
Marcelo <marceloboaria@gmail.com>
2024-06-26 19:28:51 -0300

    atualização do email para unico

--- src/main/java/com/clickcar/clickcarback/entities/User.java
+++ src/main/java/com/clickcar/clickcarback/entities/User.java
@@ -26,6 +26,7 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(unique = true)
     private String email;
     @Column(unique = true)
     private String cpf;
